b07b099f421d15075dd699211f827707020c9b87
Umer Aalam <umerrazzaq.dev@gmail.com>
2025-11-18 21:04:44 +0500

    imports-added

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,4 +1,14 @@
-export const COUNTRY_LABELS: Record<string, string> = {
+import pk from "../streams/pk.m3u?raw";
+import uk from "../streams/uk.m3u?raw";
+import usa from "../streams/us.m3u?raw";
+import ca from "../streams/ca.m3u?raw";
+import bd from "../streams/bd.m3u?raw";
+import india from "../streams/in.m3u?raw";
+import sa from "../streams/sa.m3u?raw";
+import de from "../streams/de.m3u?raw";
+import fr from "../streams/fr.m3u?raw";
+
+const COUNTRY_LABELS: Record<string, string> = {
   pk: "Pakistan",
   uk: "United Kingdom",
   us: "United States",
